4f3fe47b9d81664d9766e89628d96e0e3d78e24d
Aman Arab <aman.arabzadeh98@gmail.com>
2022-06-15 07:00:41 +0200

    Added some new find duplicate functions

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ void missingElementUnsortedArray(int A[], int H[], int l, int h, int n){
          cout << "Element missing " << i << endl;
         }
      }
-
 }
-// Checking for missing element in an array
+   
+// Checking for missing element in an arrayf
 void missingElement(int A[], int l, int h, int n){
     int diff = l-0;
     for(int i = 0; i < n; i++){
@@ -27,6 +27,28 @@ void missingElement(int A[], int l, int h, int n){
     }
 }
 
+ 
+ // Finding duplicate in a array and printing it once
+ void duplicateArr(int A[], int n){
+       for(int i = 0; i < n; i++){
+           if(A[i] == A[i+1]){
+            cout << "Duplicate  in list: " << A[i] << endl;
+           }
+       }
+ }
+
+ // Finding duplicate and counting the duplicates
+ void countDuplicate(int A[], int n){
+  int temp = 0;
+  for(int i = 0; i <n; i++){
+   if(A[i] == A[i+1]){
+    temp = i+1;
+    while(A[temp] == A[i]) temp++;
+    cout << A[i] << " is appeared " << temp -i << " times." << endl;
+    i = temp-1;
+   }
+  }
+ }
 
 int main(){
     int A[] ={6,7,8,9,11,12,15,16,17,18,19,23};
@@ -38,6 +60,14 @@ int main(){
   int H[15] = {0};
     cout << "================================" << endl << endl;
     missingElementUnsortedArray(B, H, 1, 15, size2);
+
+    cout << "=======================================" << endl << endl;
+    int C[] ={6,6,7,8,8,9,9,11,12,12,12,15,16,17,18,18,19,23,23};
+    int size3 = sizeof(C) / sizeof(int);
+    duplicateArr(C, size3);
+
+    cout << "======================================" << endl << endl;
+    countDuplicate(C, size3);
 
  return 0; 
 }
